7cda89d10a7ee6713fac2c689ed605aa7aa0a7d3
Victoria <mvgodoy97@gmail.com>
2022-09-29 19:33:21 -0300

    Proyecto casi terminado/falta solucionar data json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { TrabajemosComponent } from './componentes/trabajemos/trabajemos.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    SobreMiComponent,
+    TrabajemosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/trabajemos/trabajemos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trabajemos',
+  templateUrl: './trabajemos.component.html',
+  styleUrls: ['./trabajemos.component.css']
+})
+export class TrabajemosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
